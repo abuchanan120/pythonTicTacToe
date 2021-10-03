2998b30f195758a63e1ad6f9bb14a065c96ee68d
abuchanan120 <61256045+abuchanan120@users.noreply.github.com>
2021-10-03 14:11:05 -0400

    Errors fixed, final commit

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from random import randrange
-count = 0
-board = ['1', '2', '3'], ['4', 'X', '6'], ['7', '8', '9']
 
 
 def display_board(board):
-# The function accepts one parameter containing the board's current status
-# and prints it out to the console.
+    # The function accepts one parameter containing the board's current status
+    # and prints it out to the console.
     print("+-------+-------+-------+")
     print("|       |       |       |")
     print("|  ", board[0][0], "  |  ", board[0][1], "  |  ", board[0][2], "  |")
@@ -21,89 +19,98 @@ def display_board(board):
     print("+-------+-------+-------+")
 
 
-
 def enter_move(board):
-# The function accepts the board's current status, asks the user about their move,
-# checks the input, and updates the board according to the user's decision.
-    if count==0:
-        count+=1
-    print("Enter your move:")
-    move=int(input())
+    # The function accepts the board's current status, asks the user about their move,
+    # checks the input, and updates the board according to the user's decision.
+    global count        #count variable to keep track of moves
+    if count == 0:
+        count += 1
+    print("Enter your move:", end="")
+    move = int(input())
 
-    if move<=3:
-        if (board[0][move-1] != 'X') & (board[0][move-1] != 'O'):
+    if move <= 3:
+        if (board[0][move - 1] != 'X') & (board[0][move - 1] != 'O'): #if in the first column
             board[0][move - 1] = 'O'
             count += 1
-    elif move>3 & move<=6:
-        if board[1][move-4] != 'X' & board[1][move-4] != 'O':
-            board[1][move-4] = 'O'
+        else:
+            print("Please choose a valid move")
+    elif move <= 6:
+        if (board[1][move - 4] != 'X') & (board[1][move - 4] != 'O'): #if in the second column
+            board[1][move - 4] = 'O'
             count += 1
-    elif move>6 & move<=9:
-        if board[2][move-7] != 'X' & board[2][move-7] != 'O':
-            board[2][move-7] = 'O'
+        else:
+            print("Please choose a valid move")
+    elif move > 6 & move <= 9:
+        if (board[2][move - 7] != 'X') & (board[2][move - 7] != 'O'): #if in the third column
+            board[2][move - 7] = 'O'
             count += 1
+        else:
+            print("Please choose a valid move")
     else:
         print("Please choose a valid move")
         enter_move(board)
+    display_board(board)
+
 
 def make_list_of_free_fields(board):
-# The function browses the board and builds a list of all the free squares;
-# the list consists of tuples, while each tuple is a pair of row and column numbers.
-    list=[]
+    # The function browses the board and builds a list of all the free squares;
+    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+    list = []
     for i in range(len(board)):
         for k in range(len(board[i])):
             if (board[i][k] != 'O') & (board[i][k] != 'X'):
-                list.append((i,k))
+                list.append((int(i), int(k)))
 
 
 def victory_for(board, sign):
     # The function analyzes the board's status in order to check if
     # the player using 'O's or 'X's has won the game
+    global winner
     if count >= 5:
         if board[0][0] == board[0][1] == board[0][2] == sign:  # across the top
-            display_board(board)
+            winner = 1
             if sign == 'O':
                 print("You won!")
             else:
                 print("The Computer Won!")
         elif board[1][0] == board[1][1] == board[1][2] == sign:  # across the middle
-            display_board(board)
+            winner = 1
             if sign == 'O':
                 print("You won!")
             else:
                 print("The Computer Won!")
         elif board[2][0] == board[2][1] == board[2][2] == sign:  # across the bottom
-            display_board(board)
+            winner = 1
             if sign == 'O':
                 print("You won!")
             else:
                 print("The Computer Won!")
         elif board[0][0] == board[1][0] == board[2][0] == sign:  # down left
-            display_board(board)
+            winner = 1
             if sign == 'O':
                 print("You won!")
             else:
                 print("The Computer Won!")
         elif board[0][1] == board[1][1] == board[2][1] == sign:  # down middle
-            display_board(board)
+            winner = 1
             if sign == 'O':
                 print("You won!")
             else:
                 print("The Computer Won!")
         elif board[0][2] == board[1][2] == board[2][2] == sign:  # down right
-            display_board(board)
+            winner = 1
             if sign == 'O':
                 print("You won!")
             else:
                 print("The Computer Won!")
         elif board[0][0] == board[1][1] == board[2][2] == sign:  # top left bottom right diagonal
-            display_board(board)
+            winner = 1
             if sign == 'O':
                 print("You won!")
             else:
                 print("The Computer Won!")
-        elif board[0][0] == board[1][0] == board[2][0] == sign:  # top right bototm left diagonal
-            display_board(board)
+        elif board[0][0] == board[1][0] == board[2][0] == sign:  # top right bottom left diagonal
+            winner = 1
             if sign == 'O':
                 print("You won!")
             else:
@@ -111,26 +118,57 @@ def victory_for(board, sign):
 
             # If all spots are filled and nobody won, it's a tie
     if count == 9:
+        winner = 1
         print("It's a Tie!!")
+
 
 def draw_move(board):
     # The function draws the computer's move and updates the board.
+    global count
     make_list_of_free_fields(board)
-    draw = randrange(1,9)
-    if draw == 5:
-        draw_move(board)
-    elif draw<=3:
-        if (0, draw-1) in list:
-            board[0][draw-1] = 'X'
-        else:
+    if count < 9:
+        draw = int(randrange(1, 10))
+        if draw == 5:
             draw_move(board)
-    elif draw>3 & draw<=6:
-        if (1, draw - 4) in list:
-            board[1][draw - 4] = 'X'
+        elif draw <= 3:
+            if (board[0][draw - 1] != 'X') & (board[0][draw - 1] != 'O'):
+                board[0][draw - 1] = 'X'
+                count += 1
+            else:
+                draw_move(board)
+        elif draw <= 6:
+            if (board[1][draw - 4] != 'X') & (board[1][draw - 4] != 'O'):
+                board[1][draw - 4] = 'X'
+                count += 1
+            else:
+                draw_move(board)
         else:
-            draw_move(board)
-    else:
-        if (2, draw - 7) in list:
-            board[2][draw - 7] = 'X'
+            if (board[2][draw - 7] != 'X') & (board[2][draw - 7] != 'O'):
+                board[2][draw - 7] = 'X'
+                count += 1
+            else:
+                draw_move(board)
+        display_board(board)
+
+
+if __name__ == "__main__":
+    count = 1
+    winner = 0
+    gameboard = ['1', '2', '3'], ['4', 'X', '6'], ['7', '8', '9']
+    display_board(gameboard)
+    while (count < 9) | (winner == 0):
+        if count % 2 != 0:
+            enter_move(gameboard)
         else:
-            draw_move(board)
+            if winner != 1:
+                draw_move(gameboard)
+            else:
+                break
+        if count >= 5:
+            victory_for(gameboard, 'X')
+            if winner == 1:
+                break
+            else:
+                victory_for(gameboard, 'O')
+                if winner == 1:
+                    break
